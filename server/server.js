d3ad3969f35ab814478aeed85dfa5448c58bb4fc
fukiran <macdorphoto@yahoo.com>
2021-01-22 19:37:28 +0000

    add server.js

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+app.use(cors());
+
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+
+app.use(bodyParser.json());
+
+MongoClient.connect('mongodb://localhost:27017')
+  .then((client) => {
+    const db = client.db('mountains');
+    const munrosCollection = db.collection('munros');
+    const munrosRouter = createRouter(munrosCollection);
+    app.use('/api/munros', munrosRouter);
+  })
+  .catch(console.err);
+
+app.listen(3000, function () {
+  console.log(`Listening on port ${ this.address().port }`);
+});

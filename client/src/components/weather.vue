0a6b42d6afe205cfe821a350e374e79a11b798b9
RhiannonAW123 <rhiannonaimeewilson@gmail.com>
2021-01-24 16:11:52 +0000

    committed without key

--- client/src/components/weather.vue
+++ client/src/components/weather.vue
@@ -14,7 +14,7 @@ data () {
 props: ['id'],
 methods: {
     getWeather(locationID) {
-        key = 'b9805b0f-ca36-48a5-a61c-190441b180ed';
+        key = '';
         fetch(`http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/${locationID}?res=3hourly&key=${key}`)
         .then( res => res.json())
         .then(id => this.dataFromAPI = id);

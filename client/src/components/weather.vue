9c100534e8ec0a09f8ad3b6363a174bacd74a5de
fukiran <macdorphoto@yahoo.com>
2021-01-24 19:08:11 +0000

    weather fetched into component

--- client/src/components/weather.vue
+++ client/src/components/weather.vue
@@ -3,18 +3,24 @@
 </template>
 
 <script>
+import { eventBus } from '../main'
+
 export default {
 name: 'weather-section',
 data () {
     return {
-        dataFromAPI: []
+        dataFromAPI: [],
     }
 },
-props: ['locationId'],
+// props: ['munro'],
+mounted() {
+    eventBus.$on('selectMunro',munro => {
+        this.getWeather(munro);
+    })
+},
 methods: {
-    getWeather() {
-        key = '';
-        fetch(`http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/350373?res=3hourly&key=`)
+    getWeather(munro) {
+        fetch(`http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/${munro.metoffice_loc_id}?res=3hourly&key=`)
         .then( res => res.json())
         .then(data => this.dataFromAPI = data);
 

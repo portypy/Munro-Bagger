38d825fca4f3cd1f2b6d9e17aaa71ba0d9660473
fukiran <macdorphoto@yahoo.com>
2021-01-24 19:33:46 +0000

    add data from API

--- client/src/components/weather.vue
+++ client/src/components/weather.vue
@@ -20,7 +20,9 @@ mounted() {
 },
 methods: {
     getWeather(munro) {
-        fetch(`http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/${munro.metoffice_loc_id}?res=3hourly&key=`)
+        fetch(`http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/${munro.metoffice_loc_id}?res=3hourly&key=b9805b0f-ca36-48a5-a61c-190441b180ed
+
+`)
         .then( res => res.json())
         .then(data => this.dataFromAPI = data);
 

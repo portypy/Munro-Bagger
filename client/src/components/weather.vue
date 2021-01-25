acec7646358ad7957aafa9f386dc09052c6ca512
fukiran <macdorphoto@yahoo.com>
2021-01-25 23:10:11 +0000

    seeds update

--- client/src/components/weather.vue
+++ client/src/components/weather.vue
@@ -40,7 +40,7 @@ mounted() {
 },
 methods: {
     getWeather(munro) {
-        fetch(`http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/${munro.metoffice_loc_id}?res=daily&key=b9805b0f-ca36-48a5-a61c-190441b180ed`)
+        fetch(`http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/${munro.metoffice_loc_id}?res=daily&`)
         .then( res => res.json())
         .then(data => this.dataFromAPI = data);
 
